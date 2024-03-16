821057e1a1dd2122e3daf016879a0e6d05b3fc6f
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2024-03-16 19:59:29 +0500

    ..

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 
 const Projects = () => {
-  const targetRef = useRef(null);
-  const { scrollYProgress } = useScroll({ target: targetRef });
-  const y = useTransform(scrollYProgress, [0, 1], ["1%", "-45%"]);
-  return (
-    <div ref={targetRef} className="relative h-[300vh]">
-      <div className="sticky top-0 flex h-screen items-center overflow-hidden flex-nowrap">
-        <div className="text-[5.5rem] font-semibold leading-tight whitespace-nowrap">
-          <motion.div style={{ x: y }}>
-            <p>Illustration • Illustration • Illustration</p>
-          </motion.div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Projects</div>;
 };
 
 export default Projects;
