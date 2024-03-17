44af5bba536c5128e8886f0e37c6c6192afe2baf
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2024-03-17 15:09:54 +0500

    completed blogs tysling

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,62 +1,54 @@
 import React from "react";
 import { MdArrowOutward } from "react-icons/md";
 import { blogs } from "@/data/blog";
+import Image from "next/image";
 
 const Blogs = () => {
   return (
     <section className="container flex flex-col md:flex-row relative gap-5 mx-12">
-      <a href={blogs[0].url} className="flex-1">
-        <div
-          style={{
-            background: `url('https://framerusercontent.com/images/TzFal3pKOSQxMaLyBqk9fsOQf8M.png')`,
-          }}
-          className="rounded-md h-52 md:h-[26.75rem]"
-        >
-          <div className="p-5 text-left flex flex-col justify-between h-52 md:h-[26.75rem]">
-            <p className="bg-[#fbfbfb99] max-w-fit py-1 px-2 rounded-full text-[#354b6f] text-xs">
-              {blogs[0]?.category}
-            </p>
-            <div>
-              <p className="title md:max-w-2xl font-bold text-black pb-3">
-                {blogs[0].title}
-              </p>
-              <p className="text-sm text-white">{blogs[0].date}</p>
-            </div>
-          </div>
+      <a
+        href={blogs[0].url}
+        className="rounded-lg shadow-md bg-[#fc7e1c] bg-opacity-20 flex-1 p-5 md:p-8 flex flex-col justify-between h-52 md:h-[420px]"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <p className="tag">{blogs[0]?.category}</p>
+        <div className="flex flex-col gap-5">
+          <Image
+            src={blogs[0].image}
+            className="h-6 md:h-24 object-left object-contain"
+          />
+          <p className="text-md md:text-4xl md:max-w-2xl">{blogs[0].title}</p>
+          <p className="text-xs md:text-sm text-[#646464]">{blogs[0].date}</p>
         </div>
       </a>
       <div className="flex flex-col gap-5">
-        <a href={blogs[1].url} className="md:max-w-80">
-          <div
-            style={{
-              background: `url('https://framerusercontent.com/images/TzFal3pKOSQxMaLyBqk9fsOQf8M.png')`,
-            }}
-            className="rounded-md h-52"
-          >
-            <div className="p-5 text-left flex flex-col justify-between h-52">
-              <p className="bg-[#fbfbfb99] max-w-fit py-1 px-2 rounded-full text-[#354b6f] text-xs">
-                {blogs[1]?.category}
-              </p>
-              <div>
-                <p className="content font-semibold pb-3">{blogs[1].title}</p>
-                <p className="text-sm text-white">{blogs[1].date}</p>
-              </div>
-            </div>
+        <a
+          href={blogs[1].url}
+          className="blog-sm bg-[#f1545b] bg-opacity-20 justify-between"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p className="tag">{blogs[1]?.category}</p>
+          <div className="flex flex-col gap-2">
+            <Image
+              src={blogs[1].image}
+              className="h-6 object-left object-contain"
+            />
+            <p className="text-md">{blogs[1].title}</p>
+            <p className="text-xs text-[#646464]">{blogs[1].date}</p>
           </div>
         </a>
-        <a href="#" className="md:max-w-80">
-          <div className="rounded-md h-52 bg-[#F5F5F5]">
-            <div className="p-5 text-left flex flex-col justify-end h-52">
-              <div>
-                <p className="text-xl font-bold text-black pb-3">
-                  View All Posts
-                </p>
-                <div className="w-12 h-12 rounded-full bg-white flex items-center justify-center">
-                  <MdArrowOutward fontSize={24} />
-                </div>
-              </div>
-            </div>
+        <a
+          href="https://medium.com/@fatimamujahid01"
+          className="blog-sm bg-[#f5f5f5] justify-end gap-3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="w-12 h-12 rounded-full bg-white flex items-center justify-center">
+            <MdArrowOutward fontSize={24} />
           </div>
+          <p className="text-md">View All Posts</p>
         </a>
       </div>
     </section>
--- src/data/blog.js
+++ src/data/blog.js
@@ -1,16 +1,20 @@
+import blog1 from "@/assets/blog1.png";
+import blog2 from "@/assets/blog2.svg";
+
 const blogs = [
   {
     title: "Setting Up CI/CD for Flutter App Using Codemagic",
     url: "https://medium.com/p/836d808f2734",
     date: "Oct 18, 2023",
     category: "Flutter",
+    image: blog1,
   },
   {
-    title:
-      "Setting Up Subscriptions in Flutter with Google Play and RevenueCat",
+    title: "Setting Up Subscriptions in Flutter with RevenueCat",
     url: "https://medium.com/p/836d808f2734",
     date: "Mar 17, 2024",
     category: "Flutter",
+    image: blog2,
   },
 ];
 
