a1a8085c3dc34cd0a69a2f1fd08f72e858efec6c
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2024-04-05 18:50:05 +0500

    fixed cursor issue in mobile

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -23,13 +23,15 @@ export default function App({ Component, pageProps }) {
   return (
     <main className={alexandria.className}>
       <Layout>
-        <AnimatedCursor
-          color="33, 33, 33"
-          innerSize={14}
-          outerSize={16}
-          innerScale={1}
-          outerScale={1}
-        />
+        <div className="hidden md:block">
+          <AnimatedCursor
+            color="33, 33, 33"
+            innerSize={14}
+            outerSize={16}
+            innerScale={1}
+            outerScale={1}
+          />
+        </div>
         <Component {...pageProps} />
       </Layout>
     </main>
